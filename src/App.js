17880bb5709b787589b51b6ab7911677df30c0d3
Sarib <msssarib12@gmail.com>
2024-01-12 11:53:01 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  // counts  =  how many times are you clicking this button
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
+
+  async function fetchingData() {
+    try {
+      setLoading(true);
+
+      let resp = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+      const result = await resp.json();
+      if (result && result.products && result.products.length) {
+        setProducts((prev) => [...prev, ...result.products]);
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchingData();
+  }, [count]);
+
+  useEffect(() => {
+    if (products && products.length === 100) {
+      setDisableButton(true);
+    }
+  }, [products]);
+
+  if (loading) {
+    return <div>Loading data ! Kindly wait</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="product" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button disabled={disableButton} onClick={() => setCount(count + 1)}>
+          Load more.
+        </button>
+        {disableButton && <p>You have reached to 100 products</p>}
+      </div>
     </div>
   );
 }
